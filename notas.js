6aa21b30dfc1127ad95cc5ae3e04a2d973e8457e
Alfredo Sánchez <alfred3ks@gmail.com>
2022-09-17 01:48:19 +0200

    Configuracion inicial del proyecto.

--- notas.js
+++ notas.js
@@ -73,5 +73,35 @@ En el siguiente recurso podemos ver una representacion del evento loop:
 
 http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 
+5- Configuracion del proyecto:---
+
+- Editor de codigo VSCode.
+- Referencia a la documentacion para JS: la MDN:
+https://developer.mozilla.org/es/docs/Web/JavaScript
+- Para la API usaremos una de platzi:
+https://fakeapi.platzi.com/
+
+- Crearemos un espacio de trabajo para montar los ejemplos. Crearemos una carpeta llamada asincronismo.
+
+- Creamos nuestro package.json:
+npm init -y
+
+{
+  "name": "asincronismo",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": ["JavaScript"],
+  "author": "Alfredo Sánchez, @alfred3ks",
+  "license": "MIT"
+}
+
+Crearemos la estructura de carpetas para organizar nuestro codigo.
+
+src
+.gitignore/node_modules
 
 */
